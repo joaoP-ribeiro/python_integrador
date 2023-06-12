baca5143cc2eaba820241f789ae7ff7988f8f2f4
JP_Ribeiro <103456562+joaoP-ribeiro@users.noreply.github.com>
2023-06-12 05:44:13 -0300

    Delete excel.py

--- excel.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def excel(nome, fabricante, valor):
-    coluna_valor = {"Nome": nome, "Fabricante": fabricante, "Valor": valor}
-    dados = pd.DataFrame(data=coluna_valor)
-    return dados
-
-
